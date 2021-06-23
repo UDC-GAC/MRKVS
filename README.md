c619b2a26fcd5e34c37844986bb30da717302b49
Markos Horro <marcos.horro@udc.es>
2021-06-23 16:01:09 +0200

    Cleaning repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# random-vector-ir
+# Random vector packing generator
+
+This is just for generating random vector packing code in a brute force manner.
--- src/experimental.py
+++ src/experimental.py
@@ -16,6 +16,8 @@ import copy
 import itertools as it
 import tqdm
 import multiprocessing as mp
+import os
+import json
 import custom_mp
 import bisect
 import random
@@ -115,12 +117,6 @@ class Intrinsic:
     def render(self):
         return f"{self.output_var} = {self.__str__()};"
 
-    # @staticmethod
-    # def generate_gather(base_addr: str, vindex: list, scale=9):
-    #     vindex = ""
-    #     gather_ins = f"__m256 tmp = _mm256_i32gather_ps({base_addr},{vindex},{scale});"
-    #     return gather_ins
-
     def __init__(
         self,
         name: str,
@@ -142,22 +138,6 @@ class Intrinsic:
 
 
 loads = [
-    # Intrinsic(
-    #     "_mm_loadu_ps",
-    #     [Mem("p", "N")],
-    #     "__m128",
-    #     "float",
-    #     128,
-    #     [Mem("p", "N+3"), Mem("p", "N+2"), Mem("p", "N+1"), Mem("p", "N+0")],
-    # ),
-    # Intrinsic(
-    #     "_mm_loadu_ps1",
-    #     [Mem("p", "N")],
-    #     "__m128",
-    #     "float",
-    #     128,
-    #     [Mem("p", "N+0"), Mem("p", "N+0"), Mem("p", "N+0"), Mem("p", "N+0")],
-    # ),
     Intrinsic(
         "_mm_load_ss",
         [Mem("p", "N")],
@@ -165,6 +145,20 @@ loads = [
         "float",
         128,
         [Mem("p", "-1"), Mem("p", "-1"), Mem("p", "-1"), Mem("p", "N+0")],
+    ),
+    Intrinsic(
+        "_mm_loadu_ps",
+        [Mem("p", "N")],
+        "__m128",
+        "float",
+        128,
+        [
+            Mem("p", "N+3"),
+            Mem("p", "N+2"),
+            Mem("p", "N+1"),
+            Mem("p", "N+0"),
+        ],
+        "AVX2",
     ),
     Intrinsic(
         "_mm256_loadu_ps",
@@ -187,16 +181,16 @@ loads = [
 ]
 
 
-# shuffles = [
-#     Intrinsic(
-#         "_mm_shuffle_ps",
-#         [Mem("p", "N"), Mem("p", "N"), "IMM8"],
-#         "__m128",
-#         "float",
-#         128,
-#         [Mem("p", "N+3"), Mem("p", "N+2"), Mem("p", "N+1"), Mem("p", "N+0")],
-#     )
-# ]
+shuffles = [
+    Intrinsic(
+        "_mm_shuffle_ps",
+        [Mem("p", "N"), Mem("p", "N"), "IMM8"],
+        "__m128",
+        "float",
+        128,
+        [Mem("p", "N+3"), Mem("p", "N+2"), Mem("p", "N+1"), Mem("p", "N+0")],
+    )
+]
 
 tmp_reg = 0
 
@@ -221,7 +215,6 @@ def get_combinations(combinations):
                 addr_loaded.append(out)
                 if ins not in new_comb:
                     new_comb.append(ins)
-    # print(mp.current_process())
     if len(addr_loaded) == len(target_addr):
         return new_comb
     return []
@@ -229,7 +222,6 @@ def get_combinations(combinations):
 
 def gen_all_load_candidates(target_addr):
     all_load_candidates = []
-    # O(N*M)
     for mem_addr in target_addr:
         base_idx = mem_addr.idx
         for load_ins in loads:
@@ -241,41 +233,242 @@ def gen_all_load_candidates(target_addr):
             new_ins.args = [get_ptr(mem_addr)]
             new_ins.output_var = new_tmp_reg()
             all_load_candidates.append(new_ins)
-    # sample_size = 10 if len(all_load_candidates) > 10 else len(all_load_candidates)
-    # all_load_candidates = random.sample(all_load_candidates, sample_size)
-    # print(sample_size)
     return all_load_candidates
 
 
-def combinations_with_replacement_loads(all_load_candidates, cl=2, end=9):
+def combinations_loads(all_load_candidates, cl=2, end=9):
     global_combinations = []
     for n in range(cl, end):
         n_candidates = 1
-        niterations = sum(
-            1 for _ in it.combinations_with_replacement(all_load_candidates, n)
-        )
+        niterations = sum(1 for _ in it.combinations(all_load_candidates, n))
         if len(global_combinations) > n_candidates:
             return global_combinations
         with mp.Pool(processes=n_processes) as pool:
             for output in tqdm.tqdm(
                 pool.istarmap(
                     get_combinations,
-                    zip(it.combinations_with_replacement(all_load_candidates, n)),
+                    zip(it.combinations(all_load_candidates, n)),
                     chunksize=25000,
                 ),
                 total=niterations,
             ):
-                # if output != [] and output not in global_combinations:
                 if output != []:
                     global_combinations.append(output)
                     if len(global_combinations) > n_candidates:
                         pool.terminate()
                         return global_combinations
-                # bisect.insort(global_combinations, output)
     return global_combinations
 
 
-#################### MAIN
+def gen_shuffles(dst, A, B, mask="0b0000"):
+    return f"{dst} = _mm256_shuffle_ps({A},{B},{mask});DO_NOT_TOUCH({dst});"
+
+
+def get_cl(addresses):
+    offsets = set()
+    for addr in addresses:
+        offsets.add(int(int(addr.idx) / 8))
+    return len(offsets)
+
+
+def generate_shuffle_instructions(comb):
+    new_comb = comb
+    nloads = len(comb)
+    for n in range(2, nloads):
+        A = (
+            f"_mm256_castps128_ps256({comb[n-1].output_var})"
+            if comb[n - 1].width == 128
+            else comb[n - 1].output_var
+        )
+        B = (
+            f"_mm256_castps128_ps256({comb[n].output_var})"
+            if comb[n].width == 128
+            else comb[n].output_var
+        )
+        dst = new_tmp_reg()
+        shuffle = gen_shuffles(
+            dst,
+            A,
+            B,
+            "mask",
+        )
+        new_comb.append(shuffle)
+    return new_comb
+
+
+def get_number_slots_ordered(candidate, target):
+    v = 0
+    for n in range(len(target.idx)):
+        if candidate.idx[n] == target.idx[n]:
+            v += 1
+    return v
+
+
+def generate_swizzle_instructions(comb, target_address):
+    new_comb = comb
+    main_ins = ""
+    max_ordered = 0
+    for load in comb:
+        m = get_number_slots_ordered(load.output, target_address)
+        if m > max_ordered:
+            main_ins = load
+            max_ordered = m
+
+    inst_to_swizzle = comb - main_ins
+    for load in inst_to_swizzle:
+        new_comb.append(load)
+
+    return new_comb
+
+
+def create_performance_bench(instructions) -> str:
+    content = "#include <immintrin.h>\n"
+    content += "#define restrict __restrict\n"
+    content += '#define NO_OPT(X) asm volatile("":"+x"(X)::);\n'
+    content += "void foo(float *restrict p) {\n"
+    content += '    __asm volatile("# LLVM-MCA-BEGIN foo");'
+    for instruction in instructions:
+        if type(instruction) == Intrinsic:
+            f.write(f"    {instruction.render()}\n")
+        else:
+            f.write(f"    {instruction}\n")
+    content += '    __asm volatile("# LLVM-MCA-END foo");'
+    for instruction in instructions:
+        content += f"    NO_OPT({instruction.output_var})\n"
+    content += "}\n"
+    return content
+
+
+def fix_json() -> None:
+    with open("perf.json") as f:
+        lines = f.readlines()
+        new_lines = []
+        new_lines.append("[\n")
+        for l in lines:
+            if "not implemented" in l or "\n" == l or "Code Region" in l:
+                continue
+            if l == "}\n":
+                new_lines.append("},\n")
+            elif l == "]\n":
+                new_lines.append("],\n")
+            else:
+                new_lines.append(l)
+        if new_lines[-1] == "},\n":
+            new_lines[-1] = "}\n"
+        elif new_lines[-1] == "],\n":
+            new_lines[-1] = "]\n"
+        new_lines.append("]\n")
+    with open("perf_fixed.json", "w") as f:
+        f.writelines(new_lines)
+
+
+def compute_performance(combination) -> tuple:
+    file_content = create_performance_bench(combination)
+    with open("__tmp.c", "w") as f:
+        f.write(file_content)
+    os.system("gcc -c -O3 -S -mavx2 __tmp.c")
+    os.system("llvm-mca-12 __tmp.s -json -o perf.json")
+    fix_json()
+    with open("perf_fixed.json") as f:
+        dom = json.loads(f.read())
+    summ = dom[1]["SummaryView"]
+    ipc = summ["IPC"]
+    avg_cycles = summ["Cycles"] / summ["Iterations"]
+
+    # Be clean
+    os.system("rm __tmp.c __tmp.s perf.json perf_fixed.json")
+    return (ipc, avg_cycles)
+
+
+def generate_new_cases() -> list:
+    array = []
+    for i in range(16):
+        positions = list(range(16))
+        for j in range(i):
+            positions[-i + j] = (j + 1) * 16
+        array.append(positions)
+
+    target_addresses = []
+    offset = 0
+    for comb in array:
+        new_comb = [Mem("p", f"{offset + elem}") for elem in comb]
+        target_addresses.append(new_comb)
+    return target_addresses
+
+
+if __name__ == "__main__":
+    target_addresses = generate_new_cases()
+    case_number = 0
+    # CORE for the brute force approach
+    for target_addr in target_addresses:
+        all_candidates = gen_all_load_candidates(target_addr)
+        print(f"length candidates = {len(all_candidates)}")
+        cl = get_cl(target_addr)
+        global_combinations = combinations_loads(
+            all_candidates, cl, len(target_addr) + 1
+        )
+        if print_combinations:
+            for comb in global_combinations:
+                print("Combination")
+                for ins in comb:
+                    print(f"    {ins}")
+        print(f"total {len(global_combinations)}")
+
+        global_combinations = sorted(global_combinations)
+        new_list = list(k for k, _ in it.groupby(global_combinations))
+
+        comb_number = 0
+        # Ugly loop for creating new benchmarks
+        for comb in new_list:
+            new_comb = generate_swizzle_instructions(comb, target_addr)
+            print(f"KERNEL {case_number}-{comb_number}")
+            variables128 = []
+            variables256 = []
+            for ins in new_comb:
+                if type(ins) == Intrinsic:
+                    if ins.width == 128 and ins.output_var not in variables128:
+                        variables128.append(ins.output_var)
+                    if ins.width == 256 and ins.output_var not in variables256:
+                        variables256.append(ins.output_var)
+                else:
+                    variables256.append(ins.split(" = ")[0].strip())
+
+            with open(f"kernels/kernel_{case_number}_{comb_number}.decl.c", "w") as f:
+                f.write("int mask;\n")
+                if len(variables128) > 0:
+                    f.write("__m128 ")
+                    for v in variables128[:-1]:
+                        f.write(f"{v}, ")
+                    f.write(f"{variables128[-1]};\n")
+                if len(variables256) > 0:
+                    f.write("__m256 ")
+                    for v in variables256[:-1]:
+                        f.write(f"{v}, ")
+                    f.write(f"{variables256[-1]};\n")
+
+            with open(f"kernels/kernel_{case_number}_{comb_number}.c", "w") as f:
+                for ins in new_comb:
+                    if type(ins) == Intrinsic:
+                        f.write(f"{ins.render()}\n")
+                    else:
+                        f.write(f"{ins}\n")
+
+            with open(f"kernels/kernel_{case_number}_{comb_number}.dce.c", "w") as f:
+                if len(variables256) > 0:
+                    f.write("MARTA_AVOID_DCE(")
+                    for v in variables256[:-1]:
+                        f.write(f"{v}, ")
+                    f.write(f"{variables256[-1]});\n")
+                if len(variables128) > 0:
+                    f.write("MARTA_AVOID_DCE(")
+                    for v in variables128[:-1]:
+                        f.write(f"{v}, ")
+                    f.write(f"{variables128[-1]});\n")
+
+            comb_number += 1
+        case_number += 1
+
+
 # target_addresses = [
 #     # # 8 cl
 #     # [
@@ -361,138 +554,3 @@ def combinations_with_replacement_loads(all_load_candidates, cl=2, end=9):
 #         Mem("p", "0"),
 #     ],
 # ]
-
-array = []
-for i in range(16):
-    positions = list(range(16))
-    for j in range(i):
-        positions[-i + j] = (j + 1) * 16
-
-    array.append(positions)
-    print(positions)
-
-target_addresses = []
-offset = 0
-for comb in array:
-    # for offset in [0, 1, 2, 3, 8, 9, 11]:
-    new_comb = [Mem("p", f"{offset + elem}") for elem in comb]
-    target_addresses.append(new_comb)
-    print(new_comb)
-
-
-def gen_shuffles(dst, A, B, mask="0b0000"):
-    return f"{dst} = _mm256_shuffle_ps({A},{B},{mask});DO_NOT_TOUCH({dst});"
-
-
-def get_cl(addresses):
-    offsets = set()
-    for addr in addresses:
-        offsets.add(int(int(addr.idx) / 8))
-    return len(offsets)
-
-
-case_number = 0
-for target_addr in target_addresses:
-    all_candidates = gen_all_load_candidates(target_addr)
-    print(f"length candidates = {len(all_candidates)}")
-    cl = get_cl(target_addr)
-    global_combinations = combinations_with_replacement_loads(
-        all_candidates, cl, len(target_addr) + 1
-    )
-    if print_combinations:
-        for comb in global_combinations:
-            print("Combination")
-            for ins in comb:
-                print(f"    {ins}")
-    print(f"total {len(global_combinations)}")
-    import time
-
-    # from merge_sort_parallel import merge_sort_parallel as msort
-
-    start_time = time.monotonic()
-    global_combinations = sorted(global_combinations)
-    stop_time = time.monotonic()
-    print("seconds: ", stop_time - start_time)
-    start_time = time.monotonic()
-    new_list = list(k for k, _ in it.groupby(global_combinations))
-    print("seconds: ", time.monotonic() - start_time)
-    print(f"cleaned total {len(new_list)}")
-
-    comb_number = 0
-    for comb in new_list:
-        print(f"KERNEL {case_number}-{comb_number}")
-        new_comb = comb
-        nloads = len(comb)
-        for n in range(2, nloads):
-            # new_comb.append("#ifdef MASK_SHUFFLE")
-            # new_comb.append(f"mask = 256 >> {int(8 * random.random())};")
-            # new_comb.append("#else")
-            # new_comb.append(f"mask = {hex(int(256 * random.random()))};")
-            # new_comb.append("#endif")
-            A = (
-                f"_mm256_castps128_ps256({comb[n-1].output_var})"
-                if comb[n - 1].width == 128
-                else comb[n - 1].output_var
-            )
-            B = (
-                f"_mm256_castps128_ps256({comb[n].output_var})"
-                if comb[n].width == 128
-                else comb[n].output_var
-            )
-            dst = new_tmp_reg()
-            shuffle = gen_shuffles(
-                dst,
-                A,
-                B,
-                "mask",
-            )
-            new_comb.append(shuffle)
-
-        variables128 = []
-        variables256 = []
-        for ins in new_comb:
-            if type(ins) == Intrinsic:
-                if ins.width == 128 and ins.output_var not in variables128:
-                    variables128.append(ins.output_var)
-                if ins.width == 256 and ins.output_var not in variables256:
-                    variables256.append(ins.output_var)
-            else:
-                variables256.append(ins.split(" = ")[0].strip())
-
-        with open(f"kernels/kernel_{case_number}_{comb_number}.decl.c", "w") as f:
-            f.write("int mask;\n")
-            if len(variables128) > 0:
-                f.write("__m128 ")
-                for v in variables128[:-1]:
-                    f.write(f"{v}, ")
-                f.write(f"{variables128[-1]};\n")
-            if len(variables256) > 0:
-                f.write("__m256 ")
-                for v in variables256[:-1]:
-                    f.write(f"{v}, ")
-                f.write(f"{variables256[-1]};\n")
-
-        with open(f"kernels/kernel_{case_number}_{comb_number}.c", "w") as f:
-            for ins in new_comb:
-                if type(ins) == Intrinsic:
-                    f.write(f"{ins.render()}\n")
-                else:
-                    f.write(f"{ins}\n")
-
-        with open(f"kernels/kernel_{case_number}_{comb_number}.dce.c", "w") as f:
-            if len(variables256) > 0:
-                f.write("MARTA_AVOID_DCE(")
-                for v in variables256[:-1]:
-                    f.write(f"{v}, ")
-                f.write(f"{variables256[-1]});\n")
-            if len(variables128) > 0:
-                f.write("MARTA_AVOID_DCE(")
-                for v in variables128[:-1]:
-                    f.write(f"{v}, ")
-                f.write(f"{variables128[-1]});\n")
-
-        comb_number += 1
-    case_number += 1
-
-
-# print(len(set(global_combinations)))
