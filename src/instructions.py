d3420b65f12af8f68f7019d9344f0c6bf0f7d3e7
Markos Horro <marcos.horro@udc.es>
2021-10-06 17:39:40 +0200

    Improving

---
+++ src/instructions.py
@@ -0,0 +1,148 @@
+# Copyright 2021 Marcos Horro
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from z3 import *
+from sat.x86_sat.parse import *
+
+
+def val_to_array(val, elems=8):
+    offset_32b = 0xFFFFFFFF
+    arr = []
+    if type(val) == str:
+        val = eval(val)
+    for i in range(elems):
+        new_val = val >> 32 * i
+        arr.append(new_val & offset_32b)
+    arr.reverse()
+    return arr
+
+
+# Custom defined instructions for MACVETH
+_mm_load_ss = parse_operation(
+    "_mm_load_ss",
+    [("a", "float", "")],
+    "dst",
+    "__m128",
+    "dst[127:96] := -1; dst[95:64] := -1; dst[63:32] := -1; dst[31:0] := a;",
+)
+_mm_loadu_ps = parse_operation(
+    "_mm_loadu_ps",
+    [("a", "float", "")],
+    "dst",
+    "__m128",
+    "dst[127:96] := a+3; dst[95:64] := a+2; dst[63:32] := a+1; dst[31:0] := a;",
+)
+_mm256_loadu_ps = parse_operation(
+    "_mm256_loadu_ps",
+    [("a", "float", "")],
+    "dst",
+    "__m256",
+    "dst[255:224] := a+7; dst[223:192] := a+6; dst[191:160] := a+5; dst[159:128] := a+4; dst[127:96] := a+3; dst[95:64] := a+2; dst[63:32] := a+1; dst[31:0] := a;",
+)
+# _mm_load_ps = _mm_loadu_ps
+# _mm256_load_ps = _mm256_loadu_ps
+
+load_instructions = [
+    globals()[i] for i in dir() if i.startswith("_mm") or i.startswith("_mv")
+]
+
+_mv_insert_mem_ps = parse_operation(
+    "_mv_insert_mem_ps",
+    [("a", "__m128", ""), ("b", "float", ""), ("imm8", "const int", "")],
+    "dst",
+    "__m128",
+    """
+tmp2[127:0] := a[127:0]
+CASE (imm8[7:6]) OF
+0: tmp1[31:0] := b
+1: tmp1[31:0] := b
+2: tmp1[31:0] := b
+3: tmp1[31:0] := b
+ESAC
+CASE (imm8[5:4]) OF
+0: tmp2[31:0] := tmp1[31:0]
+1: tmp2[63:32] := tmp1[31:0]
+2: tmp2[95:64] := tmp1[31:0]
+3: tmp2[127:96] := tmp1[31:0]
+ESAC
+FOR j := 0 to 3
+	i := j*32
+	IF imm8[j%8]
+		dst[i+31:i] := 0
+	ELSE
+		dst[i+31:i] := tmp2[i+31:i]
+	FI
+ENDFOR
+    """,
+)
+
+_mv256_blend_mem_ps = parse_operation(
+    "_mv256_blend_mem_ps",
+    [("a", "__m256", ""), ("b", "float", ""), ("imm8", "const int", "")],
+    "dst",
+    "__m256",
+    """
+FOR j := 0 to 7
+	i := j*32
+	IF imm8[j]
+		dst[i+31:i] := b+j
+	ELSE
+		dst[i+31:i] := a[i+31:i]
+	FI
+ENDFOR
+dst[MAX:256] := 0
+    """,
+)
+
+_mv_blend_mem_ps = parse_operation(
+    "_mv_blend_mem_ps",
+    [("a", "__m128", ""), ("b", "float", ""), ("imm8", "const int", "")],
+    "dst",
+    "__m128",
+    """
+FOR j := 0 to 3
+	i := j*32
+	IF imm8[j]
+		dst[i+31:i] := b+j
+	ELSE
+		dst[i+31:i] := a[i+31:i]
+	FI
+ENDFOR
+    """,
+)
+
+insert_blend_instructions = [
+    globals()[i] for i in dir() if "insert" in i or "blend" in i
+]
+
+# Update intrinsics list for iterate
+full_custom_ops_list = [
+    globals()[i] for i in dir() if i.startswith("_mm") or i.startswith("_mv")
+]
+
+regex = "|".join(
+    [
+        r"_mm(256|)_set_ps",
+        r"_mm(256|)_blend_ps",
+        r"_mm_insert_ps",
+        r"_mm256_insertf128_ps",
+    ]
+)
+intrinsics = parse_whitelist("sat/data-latest.xml", regex=regex)
+
+# intrinsics.update(dict_custom_ops)
+
+# Wrappers
+set_4_float_elements = intrinsics["_mm_set_ps"]
+set_8_float_elements = intrinsics["_mm256_set_ps"]
--- src/sat.py
+++ src/sat.py
@@ -1,9 +1,17 @@
 from typing import List
-from z3 import *
-from sat.x86_sat.parse import *
+from z3 import sat
+from sat.x86_sat.evaluate import check, Var, Call
+from instructions import (
+    load_instructions,
+    _mv_insert_mem_ps,
+    insert_blend_instructions,
+    # move_instructions,
+    set_4_float_elements,
+    set_8_float_elements,
+)
 from tqdm import tqdm
 
-MIN_CANDIDATES = 5
+MIN_CANDIDATES = 2
 
 DATA_TYPE = {
     "float": 4,
@@ -11,152 +19,92 @@ DATA_TYPE = {
 }
 
 
+class Solution:
+    def __len__(self):
+        return len(self.instructions)
+
+    def __init__(self, instructions=None, model=None):
+        self.instructions = [] if instructions is None else instructions
+        self.model = model
+
+
 def max_width(dtype: str = "float", nelems: int = 4):
     return int(DATA_TYPE[dtype] * 8 * nelems)
 
 
-regex = "|".join(
-    [
-        # r"_mm(512|256|)_set_(ps|epi32|epi8)",
-        r"_mm(256|)_set_ps",
-        r"_mm(256|)_blend_ps",
-        r"_mm_insert_ps",
-        r"_mm256_insertf128_ps",
-    ]
-)
+def print_instruction(signature, output, *args):
+    instruction = f"{signature}("
+    if not signature.startswith("_mv"):
+        instruction = f"{output} = {instruction}"
+    else:
+        instruction += f"{output},"
+    for arg in args:
+        instruction += f"{arg},"
+    instruction = f"{instruction[:-1]});"
+    print(f"[BACKEND]\t{instruction}")
+    return instruction
 
 
-def val_to_array(val, elems=8):
-    offset_32b = 0xFFFFFFFF
-    arr = []
-    if type(val) == str:
-        val = eval(val)
-    for i in range(elems):
-        new_val = val >> 32 * i
-        arr.append(new_val & offset_32b)
-    arr.reverse()
-    return arr
+def get_register(args, arg, tmp_reg, solution):
+    for tmp in tmp_reg:
+        res, m = check(arg == tmp_reg[tmp])
+        # print(arg == tmp_reg[tmp], res)
+        model_ok = True
+        for i in m:
+            model_ok = model_ok and m[i] == solution.model[i]
+        if sat == res and model_ok:
+            args += [tmp]
+            break
+    return args
 
 
-# Custom defined instructions for MACVETH
-_mm_load_ss = parse_operation(
-    "_mm_load_ss",
-    [("a", "float", "")],
-    "dst",
-    "__m128",
-    "dst[127:96] := -1; dst[95:64] := -1; dst[63:32] := -1; dst[31:0] := a;",
-)
-_mm_loadu_ps = parse_operation(
-    "_mm_loadu_ps",
-    [("a", "float", "")],
-    "dst",
-    "__m128",
-    "dst[127:96] := a+3; dst[95:64] := a+2; dst[63:32] := a+1; dst[31:0] := a;",
-)
-# _mm_load_ps = _mm_loadu_ps
-_mm256_loadu_ps = parse_operation(
-    "_mm256_loadu_ps",
-    [("a", "float", "")],
-    "dst",
-    "__m256",
-    "dst[255:224] := a+7; dst[223:192] := a+6; dst[191:160] := a+5; dst[159:128] := a+4; dst[127:96] := a+3; dst[95:64] := a+2; dst[63:32] := a+1; dst[31:0] := a;",
-)
-# _mm256_load_ps = _mm256_loadu_ps
-
-# load_instructions = [i for i in dir() if i.startswith("_mm") or i.startswith("_mv")]
-load_instructions = [_mm_load_ss, _mm_loadu_ps, _mm256_loadu_ps]
-
-_mv_insert_mem_ps = parse_operation(
-    "_mv_insert_mem_ps",
-    [("a", "__m128", ""), ("b", "float", ""), ("imm8", "const int", "")],
-    "dst",
-    "__m128",
-    """
-tmp2[127:0] := a[127:0]
-CASE (imm8[7:6]) OF
-0: tmp1[31:0] := b
-1: tmp1[31:0] := b
-2: tmp1[31:0] := b
-3: tmp1[31:0] := b
-ESAC
-CASE (imm8[5:4]) OF
-0: tmp2[31:0] := tmp1[31:0]
-1: tmp2[63:32] := tmp1[31:0]
-2: tmp2[95:64] := tmp1[31:0]
-3: tmp2[127:96] := tmp1[31:0]
-ESAC
-FOR j := 0 to 3
-	i := j*32
-	IF imm8[j%8]
-		dst[i+31:i] := 0
-	ELSE
-		dst[i+31:i] := tmp2[i+31:i]
-	FI
-ENDFOR
-    """,
-)
-
-_mv256_blend_mem_ps = parse_operation(
-    "_mv256_blend_mem_ps",
-    [("a", "__m256", ""), ("b", "float", ""), ("imm8", "const int", "")],
-    "dst",
-    "__m256",
-    """
-FOR j := 0 to 7
-	i := j*32
-	IF imm8[j]
-		dst[i+31:i] := b+j
-	ELSE
-		dst[i+31:i] := a[i+31:i]
-	FI
-ENDFOR
-dst[MAX:256] := 0
-    """,
-)
-
-_mv_blend_mem_ps = parse_operation(
-    "_mv_blend_mem_ps",
-    [("a", "__m128", ""), ("b", "float", ""), ("imm8", "const int", "")],
-    "dst",
-    "__m128",
-    """
-FOR j := 0 to 3
-	i := j*32
-	IF imm8[j]
-		dst[i+31:i] := b+j
-	ELSE
-		dst[i+31:i] := a[i+31:i]
-	FI
-ENDFOR
-    """,
-)
+def get_arguments(ins, tmp_reg, solution):
+    args = []
+    for arg in ins.args:
+        if type(arg) == Var:
+            args += [solution.model[arg.name]]
+            continue
+        if type(arg) != Call:
+            args += [f"p[{arg}]"]
+            continue
+        n_args = len(args)
+        args = get_register(args, arg, tmp_reg, solution)
+        assert n_args + 1 == len(args)
+    return args
 
 
-# Update intrinsics list for iterate
-custom_ops_list = [i for i in dir() if i.startswith("_mm") or i.startswith("_mv")]
-dict_custom_ops = dict(zip(custom_ops_list, custom_ops_list))
-intrinsics = parse_whitelist("sat/data-latest.xml", regex=regex)
-intrinsics.update(dict_custom_ops)
+def generate_code(solution: Solution):
+    reg_no = 0
+    tmp_reg = {}
+    liveness = {}
+    instructions = solution.instructions
+    c_instructions = []
+    for n in range(len(instructions)):
+        ins = instructions[n]
+        args = get_arguments(ins, tmp_reg, solution)
+        output = f"r{reg_no}"
+        tmp_reg[output] = ins
+        reg_no += 1
+        liveness[reg_no] = [n, n]
+        c_instructions.append(print_instruction(ins.fn.name, output, *args))
+    print(liveness)
 
 
-def print_model(m):
-    for mi in m:
-        print(mi)
-
-
-def get_all_combinations(n_ins: int, packing, objective, dtype="float"):
+def get_all_combinations(
+    n_ins: int, packing, objective, dtype="float"
+) -> List[Solution]:
     solutions = []
 
-    def new_solutions(case: int, instructions: List, packing):
+    def new_solutions(case: int, instructions: List[Call], packing: List) -> Solution:
         result, model = check(objective == instructions[-1])
         if result == sat:
             print("ALRIGHT: ", objective == instructions[-1], model)
             for i in instructions:
                 print("\t", i)
-            return instructions
+            return Solution(instructions, model)
         else:
             if case == n_ins:
-                return []
+                return Solution()
             # keep exploring
             new_insert = _mv_insert_mem_ps(
                 instructions[-1], packing[0], Var("i" * case, "int")
@@ -176,25 +124,29 @@ def get_all_combinations(n_ins: int, packing, objective, dtype="float"):
     return solutions
 
 
-packing = [10, 9, 1, 0]
-
-
-def main(input: List):
-    vpacking = intrinsics["_mm_set_ps"](*input)
+def main(packing: List):
+    vpacking = globals()[f"set_{len(packing)}_float_elements"](*packing)
     packing.reverse()
     MAX_INS = 2 * len(packing)
     solutions_found = 0
     print(f"Searching packing combinations for: {packing}")
+    S = []
     for n_ins in range(1, MAX_INS):
         print(f"- using {n_ins:3} instruction(s)")
         R = get_all_combinations(n_ins, packing, vpacking)
         solutions_found += len(R)
+        if len(R) > 0:
+            S += [R]
         if len(R) == 0:
             print(f"\tNo solutions using {n_ins} instruction(s)")
         if solutions_found >= MIN_CANDIDATES:
             print(f"** SEARCH FINISHED WITH {solutions_found} CANDIDATES")
             break
 
-    for s in solutions_found:
-        # render(s)
-        pass
+    for s in S:
+        for c in s:
+            generate_code(c)
+    return S
+
+
+S = main([10, 9, 1, 0])
